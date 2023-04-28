859dc147a71469a051b6ca932473008bed1ca2f3
Vikas Malhotra <malhotra.vikas@gmail.com>
2023-04-28 14:32:23 -0400

    Updated the facts file and made it random fetch

--- parseEarthquakeFactJSON.js
+++ parseEarthquakeFactJSON.js
@@ -1,10 +1,15 @@
 async function parseEarthquakeFactsJSON() {
+    //url = "https://github.com/malhotra-vikas/EarthquakeFacts-Extension/blob/bc663fc8aeb77140cfc29646a9fa163947db573c/earthquake-facts.json";
+
     const res = await fetch ("earthquake-facts.json");
     const record = await res.json();
     
     console.log(record);
+    // Genrate a random fact number between 0 and 20
+    let randomNumber = Math.floor(Math.random() * 20);
 
-    document.getElementById("factOfTheDay").innerHTML=record.fact[0].data;
+
+    document.getElementById("factOfTheDay").innerHTML=record.fact[randomNumber].data;
 
 
 }
